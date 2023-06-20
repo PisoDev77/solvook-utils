92f3a27a2c6708e59d7b332b5756eb238626f18a
PisoDev77 <pisodev0427@gmail.com>
2023-06-20 17:14:39 +0900

    docs: add template of default none question sentence
    - 질문이 없는 질문들을 위해 템플릿

--- src/components/common/HelpBtn.jsx
+++ src/components/common/HelpBtn.jsx
@@ -119,19 +119,22 @@ const data = {
         . 적절한 어휘/어법을 고르시오.
       </button>,
       <button onClick={(e) => copyTargetDom(e.target)}>
-        ※. 다음 글을 읽고 물음에 답하시오.
+        ※ 다음 글을 읽고 물음에 답하시오.
       </button>,
       <button onClick={(e) => copyTargetDom(e.target)}>
         . 빈칸에 들어갈 적절한 단어를 쓰시오.
       </button>,
       <button onClick={(e) => copyTargetDom(e.target)}>
-        ". 다음 각 문장을 해석하시오."
+        . 다음 각 문장을 해석하시오.
       </button>,
       <button onClick={(e) => copyTargetDom(e.target)}>
         . 문맥 상 주어진 문장 다음에 이어질 글의 순서를 쓰시오.
       </button>,
       <button onClick={(e) => copyTargetDom(e.target)}>
         . 아래 글을 보고 문장을 재배열 하시오.
+      </button>,
+      <button onClick={(e) => copyTargetDom(e.target)}>
+        00. 다음 글을 보고 영작하시오.
       </button>,
     ],
   },
