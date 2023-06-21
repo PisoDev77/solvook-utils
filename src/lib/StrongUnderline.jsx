cd3acd59e434b1e94fdc5fa3bb991ca16b04aa31
PisoDev77 <pisodev0427@gmail.com>
2023-06-21 21:16:17 +0900

    feat: StringUnderline class created
    > 영어 본문을 문자열로 입력받아 볼드와 밑줄처리를 처리하는 클래스
    
    - 대괄호 속 볼드처리; 쏠북 규격 대괄호를 포함하여 문자열을 볼드처리

---
+++ src/lib/StrongUnderline.jsx
@@ -0,0 +1,169 @@
+/**
+ * @author Piso
+ * @file StrongUnderline.jsx
+ * @description 문자열을 볼드나 밑줄 처리하여 JSX 포맷으로 ReactElement를 반환하는 클래스
+ */
+
+/**
+ * @class StrongUnderline
+ * @description 복사한 텍스트 내용을 표준화된 형식으로 변환하는 데 도움을 주는 클래스입니다.
+ */
+export default class StrongUnderline {
+  constructor(str) {
+    this.str = str;
+    this.#replaceNumbers();
+  }
+  /**
+   * @property {string} str - 현재 문자열
+   * @description Getter and setter for the 'str' property.
+   * @returns {string} - The current string value.
+   */
+  get str() {
+    return this._str;
+  }
+
+  /**
+   * @property {string} str - The new string value to set.
+   * @description Setter for the 'str' property.
+   * @param {string} inputStr - The new string value to set.
+   */
+  set str(inputStr) {
+    this._str = inputStr;
+  }
+
+  /**
+   * @private
+   * @method #replaceNumbers
+   * @description 숫자기호를 쏠북 파싱 규격에 맞는 유니코드로 치환합니다.
+   * @returns {void}
+   */
+  #replaceNumbers() {
+    this.str
+      .replace(/➀|❶/g, "①")
+      .replace(/➁|❷/g, "②")
+      .replace(/➂|❸/g, "③")
+      .replace(/➃|❹/g, "④")
+      .replace(/➄|❺/g, "⑤");
+  }
+
+  /**
+   * @method #wrapStrong
+   * @description 원 숫자 또는 원 소문자 다음 텍스트에 볼드체와 밑줄을 적용하여 반환합니다.
+   * @returns {string} 변환된 텍스트
+   * @private
+   */
+  #wrapNextWordWithBold() {
+    return this.str.replace(
+      /([①-⑮ⓐ-ⓩ])\s*(\b\w+\b)/g,
+      `$1 <strong>$2</strong>`
+    );
+  }
+
+  /**
+   * @method #wrapStrong
+   * @description 대괄호로 둘러싸인 텍스트에 볼드체를 적용하여 반환합니다.
+   * @returns {string} 변환된 텍스트
+   * @private
+   */
+  #wrapStrong() {
+    return this._str.replace(
+      /\[(\[[^\]]*\]|[^\[\]]*)\]/g,
+      (match) =>
+        `<strong>${match
+          .replace("[", "[ ")
+          .replace("]", " ]")
+          .replace(/\/ | \//g, "/")
+          .replace("/", " / ")}</strong>`
+    );
+  }
+
+  /**
+   * @method #wrapStrongWithNumber
+   * @description 대괄호로 둘러싸인 텍스트에 볼드체와 번호를 적용하여 반환합니다.
+   * @returns {string} 변환된 텍스트
+   * @private
+   */
+  #wrapStrongWithNumber() {
+    let cnt = 1;
+    return this._str.replace(
+      /\[(\[[^\]]*\]|[^\[\]]*)\]/g,
+      (match) =>
+        `${cnt++}) <strong>${match
+          .replace("[", "[ ")
+          .replace("]", " ]")
+          .replace(/\/ | \//g, "/")
+          .replace("/", " / ")}</strong>`
+    );
+  }
+
+  /**
+   * @method extractByStrong
+   * @description Extracts text wrapped in <strong> tags or non-tagged text from the given string.
+   * @param {string} str - The input string to extract from.
+   * @returns {Array} - An array containing the extracted text with <strong> tags and trimmed non-tagged text.
+   */
+  #extractByStrong(str) {
+    const matches = str.match(/(<strong>[^<]*<\/strong>)|([^<]+)/g) ?? [];
+
+    return matches.map((match) =>
+      match.startsWith("<strong>") ? (
+        <>
+          &nbsp;<strong>{match.replace(/[<strong>|</strong>]/g, "")}</strong>
+        </>
+      ) : (
+        match.trim()
+      )
+    );
+  }
+
+  /**
+   * @method extractLineBoldByStrong
+   * @description Extracts text wrapped in <strong> tags or non-tagged text from the given string and applies underline style.
+   * @param {string} str - The input string to extract from.
+   * @returns {Array} - An array containing the extracted text with <strong> tags, wrapped in <span> with underline style, and trimmed non-tagged text.
+   */
+  #extractLineBoldByStrong(str) {
+    const matches = str.match(/(<strong>[^<]*<\/strong>)|([^<]+)/g) ?? [];
+
+    return matches.map((match) =>
+      match.startsWith("<strong>") ? (
+        <>
+          &nbsp;
+          <span
+            style={{ textDecoration: "underline" }}
+            data-mce-style="text-decoration: underline;"
+          >
+            <strong>{match.replace(/[<strong>|</strong>]/g, "")}</strong>
+          </span>
+          &nbsp;
+        </>
+      ) : (
+        match.trim()
+      )
+    );
+  }
+
+  /**
+   * @method boldSqure
+   * @description 대괄호 안에 포함된 텍스트에 볼드체 서식을 적용합니다.
+   */
+  get boldSqure() {
+    return this.#extractByStrong(this.#wrapStrong());
+  }
+
+  /**
+   * @method boldSqureWithNumber
+   * @description 대괄호 안에 포함된 텍스트에 볼드체 서식과 번호를 적용합니다.
+   */
+  get boldSqureWithNumber() {
+    return this.#extractByStrong(this.#wrapStrongWithNumber());
+  }
+
+  /**
+   * @method boldWithLine
+   * @description 텍스트에 볼드체와 밑줄을 적용합니다.
+   */
+  get boldWithLine() {
+    return this.#extractLineBoldByStrong(this.#wrapNextWordWithBold());
+  }
+}
