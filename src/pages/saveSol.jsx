891f35ef287b45da7060335d7d362525c7b5a8e4
Piso <pisodev0427@gmail.com>
2023-09-05 19:55:22 +0900

    feat: set layout of saveSol

--- src/pages/saveSol.jsx
+++ src/pages/saveSol.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 /**
  * 쏠북 스튜디오 컴포넌트(문제) 저장을 위한 페이지 컴포넌트입니다.
- * 
+ *
  * @component
  * @description 페이지이며, 저장 기능을 위한 곳입니다.
  * @returns {JSX.Element}
  */
-export default function saveSol(){
+export default function saveSol() {
+    const [str, setStr] = useState('None');
 
-    cosnt [str, steStr] = useState('None');
+    const handleBtn = () => {
+        setStr('get Unique ID from Solvvok');
+    };
 
     return (
         <>
@@ -17,5 +20,5 @@ export default function saveSol(){
             <button onClick={handleBtn}>GET</button>
             <div>{str}</div>
         </>
-    )
+    );
 }
