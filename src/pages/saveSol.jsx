1043bdb5daf6d2f11f68179922eedd52cc883f9e
Piso <pisodev0427@gmail.com>
2023-09-05 19:37:59 +0900

    feat: init saveSol
    - 쏠북 스튜디오 문제 컴포넌트 저장을 위한 페이지 컴포넌트 생성

---
+++ src/pages/saveSol.jsx
@@ -0,0 +1,15 @@
+/**
+ * 쏠북 스튜디오 컴포넌트(문제) 저장을 위한 페이지 컴포넌트입니다.
+ * 
+ * @component
+ * @description 페이지이며, 저장 기능을 위한 곳입니다.
+ * @returns {JSX.Element}
+ */
+export default function saveSol(){
+
+    return (
+        <>
+            <h1>Save Sol</h1>
+        </>
+    )
+}
