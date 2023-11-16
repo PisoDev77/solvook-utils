f3b6fda6d887738147802bad81b36fec73de8173
Piso <pisodev0427@gmail.com>
2023-11-16 14:19:50 +0900

    feat: 문항수 page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import 'react-toastify/dist/ReactToastify.css';
 import RoutesNav from './components/Routes';
 
 import * as Pages from './pages';
-import { element } from 'prop-types';
 
 // prettier-ignore
 const routes = [
   { path: "/solvook-utils", elementName: "Parsing", caption: "파싱" },
   { path: "/solvook-utils/template", elementName: "TemplateA", caption: "템플릿 A" },
   // { path: "/solvook-utils/tmp", elementName: "Tmp", caption: "TMP" },
-  // { path: "/solvook-utils/split", elementName: "Split", caption: "나누기" },
+//   { path: "/solvook-utils/split", elementName: "Split", caption: "나누기" },
   { path: "/solvook-utils/templateB", elementName: "TemplateB", caption: "템플릿 B" },
   // { path: "/solvook-utils/saveSol", elementName: "saveSol", caption: "SaveSol" },
-  // { path: "/solvook-utils/paragraph", elementName: "Paragraph", caption: "Paragraph" },
+//   { path: "/solvook-utils/paragraph", elementName: "Paragraph", caption: "Paragraph" },
+//   { path: "/solvook-utils/story-id", elementName: "StoryId", caption: "story id" },
+//   { path: "/solvook-utils/지문추출", elementName: "지문추출", caption: "지문추출" },
+  { path: "/solvook-utils/문항수", elementName: "문항수", caption: "문항수" },
 ];
 
 function App() {
@@ -27,7 +29,6 @@ function App() {
                     const $Page = Pages[elementName];
                     return <Route path={path} element={<$Page />} />;
                 })}
-                <Route path={'/solvook-utils/paragraph'} element={<Pages.Paragraph />} />
             </Routes>
             <ToastContainer autoClose={2000} />
         </BrowserRouter>
---
+++ src/pages/문항수.jsx
@@ -0,0 +1,7 @@
+export default function 문항수() {
+    return (
+        <main>
+            <h1>vadkvj</h1>
+        </main>
+    );
+}
