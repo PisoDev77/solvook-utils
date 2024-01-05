95874677bdf9be5e6353be117a96fc506a8e84a6
Pisodev77 <pisodev0427@gmail.com>
2024-01-05 15:41:47 +0900

    패러그래프 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,25 @@ const routes = [
 //   { path: "/solvook-utils/split", elementName: "Split", caption: "나누기" },
   { path: "/solvook-utils/templateB", elementName: "TemplateB", caption: "템플릿 B" },
   // { path: "/solvook-utils/saveSol", elementName: "saveSol", caption: "SaveSol" },
-//   { path: "/solvook-utils/paragraph", elementName: "Paragraph", caption: "Paragraph" },
+  { path: "/solvook-utils/paragraph", elementName: "Paragraph", caption: "Paragraph" },
 //   { path: "/solvook-utils/story-id", elementName: "StoryId", caption: "story id" },
 //   { path: "/solvook-utils/지문추출", elementName: "지문추출", caption: "지문추출" },
-  { path: "/solvook-utils/문항수", elementName: "문항수", caption: "문항수" },
+  // { path: "/solvook-utils/문항수", elementName: "문항수", caption: "문항수" },
 ];
 
 function App() {
-    return (
-        <BrowserRouter>
-            <RoutesNav routes={routes} />
-            <Routes>
-                {routes.map(({ path, elementName }) => {
-                    const $Page = Pages[elementName];
-                    return <Route path={path} element={<$Page />} />;
-                })}
-            </Routes>
-            <ToastContainer autoClose={2000} />
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<RoutesNav routes={routes} />
+			<Routes>
+				{routes.map(({ path, elementName }) => {
+					const $Page = Pages[elementName];
+					return <Route path={path} element={<$Page />} />;
+				})}
+			</Routes>
+			<ToastContainer autoClose={2000} />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,10 +7,10 @@ import Feat from './feat';
 // import PlayGround from "./playground";
 // import Make from './Make';
 // import saveSol from './saveSol';
-// import Paragraph from './paragraph';
+import Paragraph from './paragraph';
 // import StoryId from './storyId';
 // import 지문추출 from './지문추출';
 import 문항수 from './문항수';
 
-export { Parsing, TemplateA, TemplateB, Tmp, Feat, 문항수 };
+export { Paragraph, Parsing, TemplateA, TemplateB, Tmp, Feat, 문항수 };
 // export { Parsing, TemplateA, TemplateB, Tmp, Feat, Split, Make, saveSol, Paragraph, StoryId, 지문추출 };
