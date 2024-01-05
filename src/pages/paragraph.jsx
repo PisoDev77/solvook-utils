4a049c3c2fe172cac7159896134049d824cbd0a9
Pisodev77 <pisodev0427@gmail.com>
2024-01-05 15:40:19 +0900

    12ll

--- src/pages/paragraph.jsx
+++ src/pages/paragraph.jsx
@@ -1,94 +1,99 @@
 import { useState } from 'react';
 
 export default function Paragraph() {
-    const [data, setData] = useState({
-        str: '',
-        res: [],
-    });
+	const [data, setData] = useState({
+		str: '',
+		res: [],
+	});
 
-    /**
-     * 주어진 할당된 paragraph 문자열을 문장 단위로 나누어 React Element 배열을 반환하는 함수.
-     * @param {string} paragraph - paragraph 문자열
-     * @returns {Array<JSX.Element>} - React Element 배열
-     */
-    const parseParagraph = (paragraph) => {
-        const [textbookId, unitId, storyId, paragraphId, paragraphText, pageInfo, meta_data, filename] =
-            paragraph.split(/\t/g);
+	/**
+	 * 주어진 할당된 paragraph 문자열을 문장 단위로 나누어 React Element 배열을 반환하는 함수.
+	 * @param {string} paragraph - paragraph 문자열
+	 * @returns {Array<JSX.Element>} - React Element 배열
+	 */
+	const parseParagraph = (paragraph) => {
+		const [textbookId, unitId, storyId, paragraphId, paragraphText, pageInfo, meta_data, filename] =
+			paragraph.split(/\t/g);
 
-        // 문장 분리 정규식 패턴0
-        const sentencePattern = /(\.|\!|\?)/g;
+		// 문장 분리 정규식 패턴0
+		const sentencePattern = /(\.|\!|\?)/g;
 
-        // 문장을 분리하여 배열에 저장
-        const sentences = paragraphText
-            .replace(/Mr\.|Dr\./g, (match) => match.replace('.', ',,'))
-            .replace(/U\.S\./g, (match) => match.replace('.', ',,'))
-            .split(sentencePattern)
-            .map((sentence) => sentence.replace(/\n/g, '').trim());
+		// 문장을 분리하여 배열에 저장
+		const sentences = paragraphText
+			.replace(/Mr\.|Dr\./g, (match) => match.replace('.', ',,'))
+			.replace(/U\.S\./g, (match) => match.replace('.', ',,'))
+			.split(sentencePattern)
+			.map((sentence) => sentence.replace(/\n/g, '').trim());
 
-        const sentenceElements = [];
-        let formerSentence = '';
+		const sentenceElements = [];
+		let formerSentence = '';
 
-        let previousForm = [0, 0, 0];
-        let sentenceId = 0;
+		let previousForm = [0, 0, 0];
+		let sentenceId = 0;
 
-        sentences.forEach((sentence) => {
-            const isFullStop = sentencePattern.test(sentence.trim());
-            formerSentence = isFullStop ? formerSentence + sentence : sentence;
+		sentences.forEach((sentence) => {
+			const isFullStop = sentencePattern.test(sentence.trim());
+			formerSentence = isFullStop ? formerSentence + sentence : sentence;
 
-            if (isFullStop) {
-                if (+textbookId !== +previousForm[0]) sentenceId = 1;
-                else if (+unitId !== +previousForm[1]) sentenceId = 1;
-                else if (+paragraphId !== +previousForm[2]) sentenceId = 1;
-                else sentenceId++;
+			if (isFullStop) {
+				if (+textbookId !== +previousForm[0]) sentenceId = 1;
+				else if (+unitId !== +previousForm[1]) sentenceId = 1;
+				else if (+paragraphId !== +previousForm[2]) sentenceId = 1;
+				else sentenceId++;
 
-                sentenceElements.push(
-                    <tr key={sentenceElements.length}>
-                        <td>{textbookId}</td>
-                        <td>{unitId}</td>
-                        <td>{storyId}</td>
-                        <td>{paragraphId}</td>
-                        <td>{sentenceId}</td>
-                        <td>{formerSentence.replace(',,', '.')}</td>
-                        <td>{pageInfo}</td>
-                        <td>{meta_data}</td>
-                        <td>{filename}</td>
-                    </tr>
-                );
-            }
+				sentenceElements.push(
+					<tr key={sentenceElements.length}>
+						<td>{textbookId}</td>
+						<td>{unitId}</td>
+						<td>{storyId}</td>
+						<td>{paragraphId}</td>
+						<td>{sentenceId}</td>
+						<td>{formerSentence.replace(',,', '.')}</td>
+						<td>{pageInfo}</td>
+						<td>{meta_data}</td>
+						<td>{filename}</td>
+					</tr>
+				);
+			}
 
-            previousForm = [textbookId, unitId, paragraphId];
-        });
+			previousForm = [textbookId, unitId, paragraphId];
+		});
 
-        return sentenceElements;
-    };
+		return sentenceElements;
+	};
 
-    const handleForm = (e) => {
-        const { value } = e.target;
+	const handleForm = (e) => {
+		const { value } = e.target;
 
-        const paragraphs = value.split('{구분}').filter((i) => i.trim() !== '');
+		const paragraphs = value.split('{구분}').filter((i) => i.trim() !== '');
 
-        const res = paragraphs.map((paragraph, index) => (
-            <table key={index} border={1}>
-                <tbody>{parseParagraph(paragraph)}</tbody>
-            </table>
-        ));
+		// 문장 분리 정규식 패턴0
+		const sentencePattern = /(\.|\!|\?)/g;
 
-        setData({
-            str: value,
-            res,
-        });
-    };
+		// const res = paragraphs.map((paragraph) =>
+		// 	paragraph.split(sentencePattern).map((sentence) => <div>{sentence}</div>)
+		// );
 
-    return (
-        <>
-            <form onChange={handleForm}>
-                <hr />
-                <textarea value={data.str} name={'str'} style={{ resize: 'both', height: '20vh' }}></textarea>
-                <br />
-                <div style={{ display: 'flex' }}>
-                    <div style={{ width: '80%' }}>{data.res}</div>
-                </div>
-            </form>
-        </>
-    );
+		const res = paragraphs
+			.map((paragraph) => paragraph.replace(sentencePattern, (match) => match + '{Enter}'))
+			.map((paragraph) => paragraph.split('{Enter}').map((sentence) => <div>{sentence}</div>));
+
+		setData({
+			str: value,
+			res,
+		});
+	};
+
+	return (
+		<>
+			<form onChange={handleForm}>
+				<hr />
+				<textarea value={data.str} name={'str'} style={{ resize: 'both', height: '20vh' }}></textarea>
+				<br />
+				<div style={{ display: 'flex' }}>
+					<div style={{ width: '80%' }}>{data.res}</div>
+				</div>
+			</form>
+		</>
+	);
 }
