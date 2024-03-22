9e6d57f71745211196e7732edad4dfa6dcddf102
Pisodev77 <pisodev0427@gmail.com>
2024-03-22 10:29:56 +0900

    fix: unused page, file, routes are deleted.
    - 문항수
    - SaveSol
    - paragraph
    - templateB
    - tmp
    - parsing
    - templateA
    - Split
    - feat
    - Make

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,23 @@ import * as Pages from './pages';
 
 // prettier-ignore
 const routes = [
-  { path: "/solvook-utils", elementName: "Parsing", caption: "파싱" },
-  { path: "/solvook-utils/template", elementName: "TemplateA", caption: "템플릿 A" },
-  { path: "/solvook-utils/tmp", elementName: "Tmp", caption: "TMP" },
-  { path: "/solvook-utils/split", elementName: "Split", caption: "나누기" },
-  { path: "/solvook-utils/templateB", elementName: "TemplateB", caption: "템플릿 B" },
-  { path: "/solvook-utils/saveSol", elementName: "saveSol", caption: "SaveSol" },
-  { path: "/solvook-utils/paragraph", elementName: "Paragraph", caption: "Paragraph" },
-  { path: "/solvook-utils/story-id", elementName: "StoryId", caption: "story id" },
+  { path: "/solvook-utils/", elementName: "Home", caption: "Home" },
   { path: "/solvook-utils/지문추출", elementName: "지문추출", caption: "지문추출" },
-  { path: "/solvook-utils/문항수", elementName: "문항수", caption: "문항수" },
 ];
 
 function App() {
-    return (
-        <BrowserRouter>
-            <RoutesNav routes={routes} />
-            <Routes>
-                {routes.map(({ path, elementName }) => {
-                    const $Page = Pages[elementName];
-                    return <Route path={path} element={<$Page />} />;
-                })}
-            </Routes>
-            <ToastContainer autoClose={2000} />
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<RoutesNav routes={routes} />
+			<Routes>
+				{routes.map(({ path, elementName }) => {
+					const $Page = Pages[elementName];
+					return <Route path={path} element={<$Page />} />;
+				})}
+			</Routes>
+			<ToastContainer autoClose={2000} />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,3 @@
-import Parsing from './parsing';
-import TemplateA from './template';
-import TemplateB from './TemplateB';
-import Tmp from './tmp';
-import Feat from './feat';
-import Split from './Split';
-import Make from './Make';
-import saveSol from './saveSol';
-import Paragraph from './paragraph';
-import StoryId from './storyId';
 import 지문추출 from './지문추출';
-import 문항수 from './문항수';
 
-// export { Parsing, TemplateA, TemplateB, Tmp, Feat, 문항수 };
-export { Parsing, TemplateA, TemplateB, Tmp, Feat, Split, Make, saveSol, Paragraph, StoryId, 지문추출, 문항수 };
+export { 지문추출 };
