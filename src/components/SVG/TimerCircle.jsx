2a75d592b957aa04b38f1c92326182af6e98320f
Pisodev77 <pisodev0427@gmail.com>
2024-06-15 16:09:02 +0900

    asf

--- src/components/SVG/TimerCircle.jsx
+++ src/components/SVG/TimerCircle.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import throttle from 'lodash/throttle';
 
 export default function TimerCircle({
 	setMin,
@@ -12,17 +13,19 @@ export default function TimerCircle({
 	strokeWidth = 10,
 	str = '',
 }) {
+	const ref = useRef(null);
 	const [tmp, setTmp] = useState(strokeDashoffset);
+	const [isMouseDown, setIsMouseDown] = useState(false); // 클릭 상태를 나타내는 상태 변수 추가
 
 	useEffect(() => {
 		setTmp(strokeDashoffset);
-	}, [time]);
+	}, [time, strokeDashoffset]);
 
-	const handleClick = (e) => {
-		const section = e.currentTarget;
+	const getPos = (e) => {
+		const section = ref.current;
 		const rect = section.getBoundingClientRect();
-		const offsetX = e.clientX - rect.left;
-		const offsetY = e.clientY - rect.top;
+		const offsetX = (e.clientX ?? e.touches[0].clientX) - rect.left;
+		const offsetY = (e.clientY ?? e.touches[0].clientY) - rect.top;
 
 		// 중심 좌표로부터의 상대 좌표 계산
 		const relativeX = offsetX - rect.width / 2;
@@ -44,10 +47,41 @@ export default function TimerCircle({
 		}
 		setTime(seconds);
 	};
-	const handleDrag = (e) => {};
+
+	const throttledGetPos = throttle(getPos, 200); // 200ms 간격으로 호출
+
+	const handleClick = (e) => {
+		getPos(e);
+	};
+
+	const handleMouseDown = (e) => {
+		setIsMouseDown(true); // 마우스 클릭 상태로 설정
+	};
+
+	const handleMouseMove = (e) => {
+		if (isMouseDown) {
+			throttledGetPos(e); // 마우스 클릭 상태일 때만 이벤트 처리
+		}
+	};
+
+	const handleMouseUp = () => {
+		setIsMouseDown(false); // 마우스 클릭 해제 상태로 설정
+	};
+
+	const handleTouchMove = (e) => {
+		throttledGetPos(e.touches[0]); // 터치 이벤트 처리
+	};
 
 	return (
-		<section className='timer-circle' onClick={handleClick} onDrag={handleDrag}>
+		<section
+			ref={ref}
+			className='timer-circle'
+			onMouseDown={handleMouseDown}
+			onMouseMove={handleMouseMove}
+			onMouseUp={handleMouseUp}
+			onClick={handleClick}
+			onTouchMove={handleTouchMove}
+		>
 			<svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
 				<circle cx={cxy} cy={cxy} r={cxy / 2 + 10} />
 				<text x='30' y='55' className='timer-text'>
