63dfcf5499712bea46b4e25daf03fc4e463ea3fe
Pisodev77 <pisodev0427@gmail.com>
2024-06-17 22:20:49 +0900

    feat: Add Route - QSlayer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ export { default as CBS } from './CBS';
 export { default as Timer } from './Timer';
 export { default as Page404 } from './Page404';
 export { default as PlayWright } from './Playwright';
+export { default as QSlayer } from './QSlayer';
 export function Home() {
 	return (
 		<article>
