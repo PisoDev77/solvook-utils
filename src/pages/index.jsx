06a44d7c05a0b7035645f3f1075693b9831bef8f
Pisodev77 <pisodev0427@gmail.com>
2024-08-02 00:26:10 +0900

    fix: git deploy fail

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 export { default as OpenJSON } from './OpenJSON';
 export { default as CBS } from './CBS';
 export { default as Page404 } from './Page404';
-export { default as PlayWright } from './Playwright';
+// export { default as PlayWright } from './Playwright';
 export { default as Additional } from './Additional';
 export { default as Json } from './Json';
 export { default as Home } from './Home';
