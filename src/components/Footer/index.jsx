84f71c1efaf1334f13a6c85310aff668a26b8c48
Pisodev77 <pisodev0427@gmail.com>
2024-08-07 17:27:57 +0900

    refactor: footer open/close;

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,28 +4,37 @@ import JSON from './JSONIcon';
 import CBS from './CBSIcon';
 import Additional from './AdditionalIcon';
 import UiComponents from './UiComponentsIcon';
+import { useState } from 'react';
 
-export default function Footer({ routes }) {
+export default function Footer() {
 	const navigate = useNavigate();
+
+	const [visible, setVisible] = useState(true);
 
 	return (
 		<footer>
-			<nav className='footer-nav'>
-				<ul>
-					<li onClick={() => navigate('/json')}>
-						<JSON />
-					</li>
-					<li onClick={() => navigate('/cbs')}>
-						<CBS />
-					</li>
-					<li onClick={() => navigate('/additional')}>
-						<Additional />
-					</li>
-					<li onClick={() => navigate('/uicomponents')}>
-						<UiComponents />
-					</li>
-				</ul>
+			<nav className={'footer-nav ' + (visible ? '' : 'close')}>
+				{visible ? (
+					<ul>
+						<li onClick={() => navigate('/json')}>
+							<JSON />
+						</li>
+						<li onClick={() => navigate('/cbs')}>
+							<CBS />
+						</li>
+						<li onClick={() => navigate('/additional')}>
+							<Additional />
+						</li>
+						<li onClick={() => navigate('/uicomponents')}>
+							<UiComponents />
+						</li>
+						<button onClick={() => setVisible(false)}>{'>'}</button>
+					</ul>
+				) : (
+					<button onClick={() => setVisible(true)}>{'<'}</button>
+				)}
 			</nav>
+			<div className='wrapper'></div>
 		</footer>
 	);
 }
